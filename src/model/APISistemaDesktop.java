4293dce0a2f88591e83c36f0ed2a5a51531f693b
William <wddsouza@inf.ufpel.edu.br>
2016-04-26 20:51:50 -0300

    Adicionados metodos para adicionar e remover uma movimentacao a um determinado mes. Esses metodos servem pra modificar um DadoMes e entao notificar os observers de que houve uma mudanca.

--- src/model/APISistemaDesktop.java
+++ src/model/APISistemaDesktop.java
@@ -7,12 +7,13 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Observable;
 
 /**
  *
- * @author Thainan
+ * @author William
  */
-public class APISistemaDesktop {
+public class APISistemaDesktop extends Observable{
 
     private ArrayList<DadosMes> dadosMes;
     private Conversor conversor;
@@ -33,26 +34,54 @@ public class APISistemaDesktop {
         System.out.println(sisDesk.visualizaMes(new Date(2016,04,26)).getMovimentacoes().get(0).getValor());
         System.out.println(sisDesk.visualizaMes(new Date(2016,04,26)).getMovimentacoes().get(1).getValor());
     }
-    
+
     /*
         Construtor da classe
         Inicializa o array de dadosMes vazio
         Inicializa o conversor a ser utilizado para salvar os dados em XML
     */
+
     public APISistemaDesktop(){
         dadosMes = new ArrayList<>();
         conversor = new Conversor();
     }
-    
-    /* 
-        criaMes é responsavel por adicionar um novo mes com dados ao sistema
-        e salvar esse novo mes no arquivo
+
+    /*
+        criaMes é responsavel por adicionar um novo mes com dados ao sistema.
     */
     public void criaMes(DadosMes dMes){
         dadosMes.add(dMes);
-        this.salvaMes(dMes.getMes());
     }
     
+    /* 
+        addMovimentacao adiciona uma movimentacao a um determinado mes.
+        Observer nota essa modificacao e eh notificado.
+    */
+    public void addMovimentacao(Date mes, Movimentacao mov){
+        
+        for (DadosMes dm : dadosMes){
+            if (dm.getMes().equals(mes)) {
+                dm.addMovimentacao(mov);
+                notifyObservers();
+                break;
+            }
+        }        
+    }
+    
+    /* 
+        addMovimentacao remove uma movimentacao a um determinado mes.
+        Observer nota essa modificacao e eh notificado.
+    */
+    public void removeMovimentacao(Date mes, Movimentacao mov) {
+        for (DadosMes dm : dadosMes){
+            if (dm.getMes().equals(mes)) {
+                dm.getMovimentacoes().remove(mov);
+                notifyObservers();
+                break;
+            }
+        }
+    }
+
     /* 
         visualizaMes encontra os dados de um determinado mes no sistema e envia
         para a interface para ser mostrado ao usuario
@@ -68,7 +97,7 @@ public class APISistemaDesktop {
         
         return null;
     }
-    
+
     /*
         exportaMes busca os dados do mes a ser exportado e chama o conversor
         para converter os dados para XML e o arquivo é salvo no dir recebido por
@@ -78,10 +107,11 @@ public class APISistemaDesktop {
         for (DadosMes dm : dadosMes){
             if (dm.getMes().equals(mes)) {
 //                conversor.converteParaXML(dm, dir);
+                break;
             }
         }
     }
-    
+
     /*
         salvaMes busca os dados do mes a ser exportado e chama o conversor
         para converter os dados para XML e o arquivo é salvo no dir padrao de
@@ -91,10 +121,11 @@ public class APISistemaDesktop {
         for (DadosMes dm : dadosMes){
             if (dm.getMes().equals(mes)) {
 //                conversor.converteParaXML(dm);
+                break;
             }
         }
     }
-    
+
     /*
         deletaMes busca o mes selecionado e exclui ele do sistema apagando
         junto o arquivo correspondente, caso o usuario deseje apagar aqueles
@@ -105,18 +136,18 @@ public class APISistemaDesktop {
             if (dm.getMes().equals(mes)) {
                 this.deletaArquivo(mes);
                 dadosMes.remove(dm);
-                
+                break;
             }
         }
     }
-    
+
     /*
         deletaArquivo exclui o arquivo correspondente ao mes selecionado.
     */
     public void deletaArquivo(Date mes){
         
     }
-    
+
     /*
         carregaArquivos transforma todos os arquivos existentes no diretorio
         de backup e importa para dentro do sistema em estruturas DadosMes.
@@ -124,7 +155,7 @@ public class APISistemaDesktop {
     public void carregaArquivos(){
         
     }
-    
+
     /*
         geraRelatorio
         A SER DEFINIDO
