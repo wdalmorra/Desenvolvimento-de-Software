18ce3c5c196a44ff21d2cf4de6dcceca713b5794
Eliezer <Eliezer@192.168.25.11>
2016-04-26 15:31:47 -0300

    esquema da tela Menu e tela novoMes.

--- src/view/APIView.java
+++ src/view/APIView.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package view;
 
 /**
  *
- * @author willian
+ * @author Eliezer
  */
-public class APIView {
+public interface APIView {
     
 }
