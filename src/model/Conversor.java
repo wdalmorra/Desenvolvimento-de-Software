fa63317ce4780acd2da14aaf46d52398f113e2e1
lucasmsx <lmdsxavier@inf.ufpel.edu.br>
2016-04-26 21:24:43 -0300

    Versão inicial do conversor XML -> dadosMês. (Nota: ignora categorias de receita/despesa por enquanto. Usa DEFAULT para todas as movimentações)

--- src/model/Conversor.java
+++ src/model/Conversor.java
@@ -5,10 +5,80 @@
  */
 package model;
 
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+
+
 /**
  *
  * @author Thainan
  */
 public class Conversor {
+    private String defaultPath;
     
+    public String toXML(DadosMes dadosMes) {
+        return "";
+    }
+    
+    public DadosMes fromXML(String xml) {
+        return null;
+    }
+    
+    public Conversor() {
+        this.defaultPath = ".";
+    }
+    
+    public Conversor(String defaultPath) {
+        this.defaultPath = defaultPath;
+    }
+    
+    public void converteParaXML(DadosMes dadosMes) {
+    }
+    
+    public void converteParaXML(DadosMes dadosMes, String path) {
+    }
+    
+    public DadosMes converteParaDadosMes(String path) {
+        
+        DadosMes retval = new DadosMes();
+        
+        try {
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc = builder.parse(new File(path));
+                
+                Element financas = doc.getDocumentElement();
+                NodeList dadosMensais = doc.getElementsByTagName("dadosmes");
+                
+                if (dadosMensais.getLength() > 1) {
+                   System.out.println("Warning: arquivo com multiplos meses, lendo somente o primeiro...");
+                }
+
+                Element dadosMes = (Element) dadosMensais.item(0);
+                NodeList movimentacoes = dadosMes.getElementsByTagName("movimentacao");
+                
+                // Não, não aceita foreach.
+                for (int i = 0; i < movimentacoes.getLength(); i++) {
+                    Element movimentacao = (Element) movimentacoes.item(i);
+                    int valor = Integer.parseInt(movimentacao.getAttribute("valor"));
+                    String categoria = movimentacao.getAttribute("categoria");
+                    String tipo = movimentacao.getAttribute("tipo");
+                    
+                    if (tipo.equals("receita")) {
+                        retval.addMovimentacao(new Receita(CategoriaReceita.DEFAULT, valor));
+                    } else if (tipo.equals("despesa")) {
+                        retval.addMovimentacao(new Despesa(CategoriaDespesa.DEFAULT, valor));
+                    } else {
+                        retval = null;
+                        throw new Exception();
+                    }
+                }
+
+        } catch (Exception e) {
+                System.out.println("Erro na leitura do arquivo " + path);
+        }
+        
+        return retval;
+    }
 }
