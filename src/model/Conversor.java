60197e190fa8818feaa9c040c51065df5c00b967
lucasmsx <lmdsxavier@inf.ufpel.edu.br>
2016-04-28 22:45:22 -0300

    Refatorando o conversor XML <-> DadosMês

--- src/model/Conversor.java
+++ src/model/Conversor.java
@@ -6,6 +6,7 @@
 package model;
 
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.*;
@@ -21,149 +22,182 @@ import java.util.*;
 public class Conversor {
     
     // Métodos e atributos da classe, para implementar o padrão singleton
-    static Conversor singleton;
-    static String defaultSavePath = ".";
+    private static Conversor singleton;
+    private static String caminhoPadrao = ".";
     
-    public static Conversor getInstance() {
+    public static synchronized Conversor getInstance() {
         if (singleton == null) {
-            singleton = new Conversor(defaultSavePath);
+            singleton = new Conversor(caminhoPadrao);
         }
         return singleton;
     }
     
     // Atributos e métodos do objeto Conversor
-    private String currentSavePath;
+    private String caminhoBase;
     
     private Conversor() {
-        this.currentSavePath = ".";
+        this.caminhoBase = ".";
     }
     
-    private Conversor(String savePath) {
-        this.currentSavePath = savePath;
+    private Conversor(String caminhoBase) {
+        this.caminhoBase = caminhoBase;
+    }
+    
+    private Document montaArvoreXML(DadosMes dadosMes) 
+            throws ParserConfigurationException 
+    {
+        // Prepara o documento
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document documentoXML = builder.newDocument();
+
+        // Elemento raiz: financas
+        Element elemRaiz = documentoXML.createElement("financas");
+        documentoXML.appendChild(elemRaiz);
+
+        // Elemento dadosMes: acerta a data
+        GregorianCalendar mesSubmetido = dadosMes.getMes();
+        Element elemDadosMes = documentoXML.createElement("dadosmes");
+        elemDadosMes.setAttribute(
+                "mes",
+                Integer.toString(mesSubmetido.get(GregorianCalendar.MONTH))
+        );
+        elemDadosMes.setAttribute(
+                "ano",
+                Integer.toString(mesSubmetido.get(GregorianCalendar.YEAR))
+        );
+        elemRaiz.appendChild(elemDadosMes);
+
+        // Preenche o dadosMes com as movimentacoes
+        ArrayList<Movimentacao> movimentacoes = dadosMes.getMovimentacoes();
+
+        for (Movimentacao m : movimentacoes) {
+            Element elemMovimentacao = documentoXML.createElement("movimentacao");
+            elemMovimentacao.setAttribute("valor", Integer.toString(m.getValor()));
+
+            if (m instanceof Receita) {
+                elemMovimentacao.setAttribute("tipo", "receita");
+                CategoriaReceita c = ((Receita) m).getCategoria();
+                elemMovimentacao.setAttribute("categoria", CategoriaReceita.categoriaToString(c));
+            } else if (m instanceof Despesa) {
+                elemMovimentacao.setAttribute("tipo", "despesa");
+                CategoriaDespesa d = ((Despesa) m).getCategoria();
+                elemMovimentacao.setAttribute("categoria", CategoriaDespesa.categoriaToString(d));
+            } else {
+                elemMovimentacao.setAttribute("categoria", "DEFAULT");
+            }
+            elemDadosMes.appendChild(elemMovimentacao);
+        }
+
+        return documentoXML;
+    }
+    
+    private DadosMes parserArvoreXML(Document arvoreXML) throws Exception {
+        
+        DadosMes dadosMes = new DadosMes();
+        Element financas = arvoreXML.getDocumentElement();
+        NodeList elDadosMensais = arvoreXML.getElementsByTagName("dadosmes");
+
+        if (elDadosMensais.getLength() > 1) {
+           System.out.println("Warning: arquivo com multiplos meses, lendo somente o primeiro...");
+        }
+
+        Element elDadosMes = (Element) elDadosMensais.item(0);
+        int mes = Integer.parseInt(elDadosMes.getAttribute("mes"));
+        int ano = Integer.parseInt(elDadosMes.getAttribute("ano"));
+        NodeList movimentacoes = elDadosMes.getElementsByTagName("movimentacao");
+
+        GregorianCalendar date = new GregorianCalendar(ano, mes, 1);
+        // Dia primeiro porque soh interessa o mes e o ano, os dados sao
+        // mensais
+        dadosMes.setDate(date);
+
+        // Não, não aceita foreach.
+        for (int i = 0; i < movimentacoes.getLength(); i++) {
+            Element movimentacao = (Element) movimentacoes.item(i);
+            int valor = Integer.parseInt(movimentacao.getAttribute("valor"));
+            String categoria = movimentacao.getAttribute("categoria");
+            String tipo = movimentacao.getAttribute("tipo");
+
+            if (tipo.equals("receita")) {
+                CategoriaReceita c = CategoriaReceita.stringToCategoria(categoria);
+                dadosMes.addMovimentacao(new Receita(c, valor));
+            } else if (tipo.equals("despesa")) {
+                CategoriaDespesa c = CategoriaDespesa.stringToCategoria(categoria);
+                dadosMes.addMovimentacao(new Despesa(c, valor));
+            } else {
+//                dadosMes = null;
+              throw new Exception();
+            }
+        }
+        
+        return dadosMes;
+    }
+    
+    private void gravaArquivoXML(Document arvoreXML, String caminho)
+            throws TransformerException 
+    {
+        // Cria o arquivo
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource fonte = new DOMSource(arvoreXML);
+        StreamResult fileResult = new StreamResult(new File(caminho));
+        transformer.transform(fonte, fileResult);
+    }
+    
+    private Document leArquivoXML(String caminho)
+            throws ParserConfigurationException, SAXException, IOException
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(new File(caminho));
     }
     
     public void converteParaXML(DadosMes dadosMes) {
+        // Monta o caminho do arquivo a partir dos padroes e do mes submetido
+        GregorianCalendar infoData = dadosMes.getMes();
+        String caminho = 
+                caminhoBase +
+                "/" + 
+                "dados_m" + infoData.get(GregorianCalendar.MONTH) +
+                "_a" + infoData.get(GregorianCalendar.YEAR) +
+                ".xml";
         
-        String mes = Integer.toString(
-                dadosMes.getMes().get(GregorianCalendar.MONTH)
-        );
-        
-        String ano = Integer.toString(
-                dadosMes.getMes().get(GregorianCalendar.YEAR)
-        );
-        
-        converteParaXML(dadosMes, currentSavePath + "/" + "dados_m" + mes + "_a" + ano + ".xml");
-        
+        // Chama a funcao converteParaXML(DadosMes, String) com o caminho
+        // montado como parametro
+        converteParaXML(dadosMes, caminho);
     }
     
-    public void converteParaXML(DadosMes dadosMes, String path) {
-        
+    public void converteParaXML(DadosMes dadosMes, String caminho) {
         try {
-            // Prepara o documento
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.newDocument();
-            
-            // Elemento raiz
-            Element financasElement = doc.createElement("financas");
-            doc.appendChild(financasElement);
-            
-            // Elemento dadosMes
-            Element dadosMesElement = doc.createElement("dadosmes");
-            GregorianCalendar date = dadosMes.getMes();
-            int mes = date.get(GregorianCalendar.MONTH);
-            int ano = date.get(GregorianCalendar.YEAR);
-            dadosMesElement.setAttribute("mes",Integer.toString(mes));
-            dadosMesElement.setAttribute("ano",Integer.toString(ano));
-            financasElement.appendChild(dadosMesElement);
-            
-            // Preenche o dadosMes com as movimentacoes
-            ArrayList<Movimentacao> movimentacoes = dadosMes.getMovimentacoes();
-            
-            for (Movimentacao m : movimentacoes) {
-                Element mElement = doc.createElement("movimentacao");
-                mElement.setAttribute("valor", Integer.toString(m.getValor()));
-                
-                if (m instanceof Receita) {
-                    mElement.setAttribute("tipo", "receita");
-                    CategoriaReceita c = ((Receita) m).getCategoria();
-                    mElement.setAttribute("categoria", CategoriaReceita.categoriaToString(c));
-                } else if (m instanceof Despesa) {
-                    mElement.setAttribute("tipo", "despesa");
-                    CategoriaDespesa d = ((Despesa) m).getCategoria();
-                    mElement.setAttribute("categoria", CategoriaDespesa.categoriaToString(d));
-                } else {
-                    mElement.setAttribute("categoria", "DEFAULT");
-                }
-                dadosMesElement.appendChild(mElement);
-            }
-            
-            // Cria o arquivo
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult fileResult = new StreamResult(new File(path));
-            transformer.transform(source, fileResult);
-            
-        } catch (Exception e) {
-                System.out.println("Erro na escrita do arquivo " + path);
+            Document doc = montaArvoreXML(dadosMes);
+            gravaArquivoXML(doc, caminho);
+        } catch (ParserConfigurationException | TransformerException e) {
+            System.out.println("Erro na escrita do arquivo " + caminho);
         }
-        
     }
     
-    public DadosMes converteParaDadosMes(GregorianCalendar c) {
-        String mes = Integer.toString(c.get(GregorianCalendar.MONTH));
-        String ano = Integer.toString(c.get(GregorianCalendar.YEAR));
-        return converteParaDadosMes(currentSavePath + "/" + "dados_m" + mes + "_a" + ano + ".xml");
+    public DadosMes converteParaDadosMes(GregorianCalendar infoData) {
+        String caminho = 
+                caminhoBase +
+                "/" + 
+                "dados_m" + infoData.get(GregorianCalendar.MONTH) +
+                "_a" + infoData.get(GregorianCalendar.YEAR) +
+                ".xml";
+        
+        return converteParaDadosMes(caminho);
     }
     
-    public DadosMes converteParaDadosMes(String path) {
+    public DadosMes converteParaDadosMes(String caminho) {
         
-        DadosMes retval = new DadosMes();
+        DadosMes retval = null;
         
         try {
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder builder = factory.newDocumentBuilder();
-                Document doc = builder.parse(new File(path));
-                
-                Element financas = doc.getDocumentElement();
-                NodeList dadosMensais = doc.getElementsByTagName("dadosmes");
-                
-                if (dadosMensais.getLength() > 1) {
-                   System.out.println("Warning: arquivo com multiplos meses, lendo somente o primeiro...");
-                }
-
-                Element dadosMes = (Element) dadosMensais.item(0);
-                int mes = Integer.parseInt(dadosMes.getAttribute("mes"));
-                int ano = Integer.parseInt(dadosMes.getAttribute("ano"));
-                NodeList movimentacoes = dadosMes.getElementsByTagName("movimentacao");
-                
-                GregorianCalendar date = new GregorianCalendar(ano, mes, 1);
-                // Dia primeiro porque soh interessa o mes e o ano, os dados sao
-                // mensais
-                retval.setDate(date);
-                
-                // Não, não aceita foreach.
-                for (int i = 0; i < movimentacoes.getLength(); i++) {
-                    Element movimentacao = (Element) movimentacoes.item(i);
-                    int valor = Integer.parseInt(movimentacao.getAttribute("valor"));
-                    String categoria = movimentacao.getAttribute("categoria");
-                    String tipo = movimentacao.getAttribute("tipo");
-                    
-                    if (tipo.equals("receita")) {
-                        CategoriaReceita c = CategoriaReceita.stringToCategoria(categoria);
-                        retval.addMovimentacao(new Receita(c, valor));
-                    } else if (tipo.equals("despesa")) {
-                        CategoriaDespesa c = CategoriaDespesa.stringToCategoria(categoria);
-                        retval.addMovimentacao(new Despesa(c, valor));
-                    } else {
-                        retval = null;
-                        throw new Exception();
-                    }
-                }
-                
+            Document doc = leArquivoXML(caminho);
+            retval = parserArvoreXML(doc);
         } catch (Exception e) {
-                System.out.println("Erro na leitura do arquivo " + path);
+            System.out.println("Erro na leitura do arquivo " + caminho);
+            retval = null;
         }
         
         return retval;
