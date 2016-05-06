cf6d4c20382bffbfb5d6ad4afb38ce83da75dbd4
lucasmsx <lmdsxavier@inf.ufpel.edu.br>
2016-05-06 18:08:21 -0300

    Detalhando a documentação da classe APISistemaDesktop e colocando no formato Javadoc.

--- src/model/APISistemaDesktop.java
+++ src/model/APISistemaDesktop.java
@@ -13,7 +13,30 @@ import java.util.Observable;
 
 
 /**
- *
+ * Classe de fachada que centraliza todas as operacoes de manipulacao das
+ * estruturas de dados basicas. Abrange:
+ * <ul>
+ *  <li> Manutencao das estruturas de dados que representam despesas e receitas
+ *  <li> Provimento da API para acesso a camada de modelo da aplicacao
+ *  <li> Direcionamento das requisicoes recebidas pela API para as classes
+ *       apropriadas.
+ * </ul>
+ * 
+ * <p> Alem de manter estruturas de dados para informacoes historicas salvas pelo
+ * usuario, mantem tambem uma estrutura temporaria para representar dados sendo
+ * editados no momento.
+ * 
+ * <p> A classe implementa o padrao observable. Observers sao notificados de:
+ * <ol>
+ *  <li> Mudancas na estrutura de dados temporaria
+ *  <li> Estruturas de dados condensando informacoes para geracao de relatorios,
+ *       solicitados pelo metodo geraRelatorios.
+ * </ol>
+ * 
+ * <p> Nota: mes corrente, ao longo da documentacao dos metodos desta classe, faz
+ * referencia ao mes sendo editado pela interface da aplicacao (estrutura 
+ * de dados temporaria), e nao necessariamente ao mes atual.
+ * 
  * @author William
  */
 public class APISistemaDesktop extends Observable{
@@ -23,15 +46,12 @@ public class APISistemaDesktop extends Observable{
     
     private DadosMes dadosDeTrabalho;
 
-    public static void main(String[] args) {
-        
-        
-    }
     
-    /*
-        carregaArquivos transforma todos os arquivos existentes no diretorio
-        de backup e importa para dentro do sistema em estruturas DadosMes.
-    */
+    /**
+     * Localiza todos os arquivos existentes no diretorio padrao de backup e
+     * importa as informacoes desses arquivos como estruturas de dados 
+     * acessiveis a aplicacao.
+     */
     private void carregaArquivos(){
         ArrayList<GregorianCalendar> listaDeArquivos = 
                 conversor.carregaListaDeArquivos();
@@ -46,9 +66,12 @@ public class APISistemaDesktop extends Observable{
         }
     }
     
-    /*
-        transforma a estrutura <code>Array<DadosMes></code> em arquivos xml
-    */
+    
+    /**
+     * Transforma em arquivos XML todos os dados disponiveis na estrutura
+     * de dados que representa despesas e receitas. Efetivamente grava no
+     * disco todos os dados trabalhados em memoria pela aplicacao.
+     */
     private void salvaArquivos(){
         ArrayList<GregorianCalendar> listaDeArquivos = new ArrayList<>();
         
@@ -61,11 +84,9 @@ public class APISistemaDesktop extends Observable{
     }
     
 
-    /*
-        Construtor da classe
-        Inicializa o array de dadosMes vazio
-        Inicializa o conversor a ser utilizado para salvar os dados em XML
-    */
+    /**
+     * Construtor da classe.
+     */
     public APISistemaDesktop(){
         dadosMes = new ArrayList<>();
         conversor = Conversor.getInstance();
@@ -73,18 +94,38 @@ public class APISistemaDesktop extends Observable{
         carregaArquivos();
     }
 
-    /*
-        criaMes é responsavel por adicionar um novo mes com dados ao sistema.
-    */
+    
+    /**
+     * Prepara o sistema para receber dados de movimentacao para um novo mes.
+     * Deve ser chamado antes da primeira chamada a addMovimentacao para
+     * inicializar apropriadamente as estruturas de dados necessarias.
+     * 
+     * @param gc Informa o mes e o ano para os novos dados.
+     */
     public void criaMes(GregorianCalendar gc){
         dadosDeTrabalho = new DadosMes();
         dadosDeTrabalho.setDate(gc);
     }
+
     
-    /* 
-        addMovimentacao adiciona uma movimentacao a um determinado mes.
-        Observer nota essa modificacao e eh notificado.
-    */
+    /**
+     * Adiciona uma nova entrada movimentacao no mes corrente, com informacoes
+     * de categoria e valor. Para incorporacao correta dos dados, chamadas a
+     * este metodo devem ser precedidas por pelo menos uma chamada a criaMes
+     * ou visualizaMes. Notifica observers com uma versao atualizada da
+     * estrutura de dados que representa as despesas e receitas para o mes
+     * corrente.
+     * 
+     * @param valor Quantia gasta (ou recebida) referente a categoria em
+     *              questao.
+     * @param cat   Categoria de despesa ou receita a qual os dados sao 
+     *              referentes.
+     * @param tipo  "D" para despesa ou "R" para receita.
+     * @param mes   Estrutura calendario indicando o mes e o ano sob os quais a
+     *              despesa deve ser registrada.
+     * @see criaMes
+     * @see visualizaMes
+     */
     public void addMovimentacao(int valor, String cat, String tipo, GregorianCalendar mes){
         Movimentacao mov = (tipo.equals("D")) ? 
             new Despesa(CategoriaDespesa.stringToCategoria(cat.toUpperCase()), valor) :
@@ -112,10 +153,26 @@ public class APISistemaDesktop extends Observable{
         this.notifyObservers(dadosDeTrabalho.getMovimentacoes());     
     }
     
-    /* 
-        alteraMovimentacao altera uma movimentacao a um determinado mes.
-        Observer nota essa modificacao e eh notificado.
-    */
+    
+    /**
+     * Altera dados de uma entrada movimentacao ja existente, alterando
+     * informacoes de valor referentes a uma categoria para a qual ja existem
+     * dados. Para incorporacao correta dos dados, chamadas a
+     * este metodo devem ser precedidas por pelo menos uma chamada a criaMes
+     * ou visualizaMes. Notifica observers com uma versao atualizada da
+     * estrutura de dados que representa as despesas e receitas para o mes
+     * corrente.
+     * 
+     * @param valor Quantia gasta (ou recebida) referente a categoria em
+     *              questao.
+     * @param cat   Categoria de despesa ou receita a qual os dados sao 
+     *              referentes.
+     * @param tipo  "D" para despesa ou "R" para receita.
+     * @param mes   Estrutura calendario indicando o mes e o ano sob os quais a
+     *              despesa deve ser registrada.
+     * @see criaMes
+     * @see visualizaMes
+     */
     public void alteraMovimentacao(int valor, String cat, String tipo, GregorianCalendar mes){
         Movimentacao mov = (tipo.equals("D")) ? 
             new Despesa(CategoriaDespesa.stringToCategoria(cat.toUpperCase()), valor) :
@@ -137,21 +194,32 @@ public class APISistemaDesktop extends Observable{
         this.notifyObservers(dadosDeTrabalho.getMovimentacoes());
     }
     
-    /* 
-        addMovimentacao remove uma movimentacao a um determinado mes.
-        Observer nota essa modificacao e eh notificado.
-    */
+    
+    /**
+     * Suprime uma entrada de movimentacao do mes corrente, eliminando
+     * informacoes de valor e categoria. Notifica observers com uma versao
+     * atualizada da estrutura de dados que representa as despesas e receitas
+     * para o mes corrente.
+     * 
+     * @param mes Indica mes e ano do qual as informacoes devem ser suprimidas
+     * @param mov Entrada que deve ser suprimida da estrutura de dados.
+     */
     public void removeMovimentacao(GregorianCalendar mes, Movimentacao mov) {
         dadosDeTrabalho.getMovimentacoes().remove(mov);
         this.setChanged();
         this.notifyObservers(dadosDeTrabalho.getMovimentacoes());
     }
 
-    /* 
-        visualizaMes encontra os dados de um determinado mes no sistema e envia
-        para a interface para ser mostrado ao usuario
-        retorna null caso o mes nao exista 
-    */
+    
+    /**
+     * Busca dados de um determinado mes e torna o mes especificado o mes
+     * corrente, se existirem dados para o mes especificado. Notifica
+     * observers com os dados de movimentacao para o mes corrente.
+     * 
+     * @param mes Indica o mes e o ano que devem ser buscados na base de dados.
+     * @return true, caso o mes especificado seja encontrado; false, caso
+     *         contrario.
+     */
     public boolean visualizaMes(GregorianCalendar mes){
         for (DadosMes dm : dadosMes){
             if (comparaMeses(dm.getMes(),mes)) {
@@ -168,20 +236,29 @@ public class APISistemaDesktop extends Observable{
         return false;
     }
 
-    /*
-        exportaMes busca os dados do mes a ser exportado e chama o conversor
-        para converter os dados para XML e o arquivo é salvo no dir recebido por
-        parametro.
-    */
+    
+    /**
+     * Produz um arquivo XML representando os dados de despesa e receita para
+     * um mes especifico. A localizacao do arquivo e arbitraria.
+     * 
+     * @param mes Inidica o mes e o ano a partir do qual deve ser produzido
+     *            o arquivo XML.
+     * @param dir Inidica o caminho para gravacao do arquivo (diretorio, nome
+     *            de arquivo e extensao).
+     */
     public void exportaMes(GregorianCalendar mes, String dir){
         conversor.converteParaXML(dadosDeTrabalho, dir);
     }
 
-    /*
-        salvaMes busca os dados do mes a ser exportado e chama o conversor
-        para converter os dados para XML e o arquivo é salvo no dir padrao de
-        backup dos dados.
-    */
+    
+    /**
+     * Atualiza os arquivos com os dados de despesas e receitas do mes corrente,
+     * tornando persistentes as alteracoes realizadas. Atualiza tambem as
+     * estruturas de dados internas da camada de modelo. Os arquivos sao salvos
+     * no diretorio padrao de backup.
+     * 
+     * @param mes Indica o mes e o ano a que se referem os dados de trabalho.
+     */
     public void salvaMes(GregorianCalendar mes){
         
         conversor.converteParaXML(dadosDeTrabalho);
@@ -203,11 +280,15 @@ public class APISistemaDesktop extends Observable{
         salvaArquivos();
     }
 
-    /*
-        deletaMes busca o mes selecionado e exclui ele do sistema apagando
-        junto o arquivo correspondente, caso o usuario deseje apagar aqueles
-        dados da máquina.
-    */
+    
+    /**
+     * Exclui as informacoes de um mes especifico, removendo elas das estruturas
+     * de dados internas e suprimindo o arquivo em disco da lista de arquivos
+     * indexados.
+     * 
+     * @param mes Informa mes e ano a que se referem os dados que devem ser
+     *            suprimidos.
+     */
     public void deletaMes(GregorianCalendar mes){
         for (DadosMes dm : dadosMes){
             if (comparaMeses(dm.getMes(),mes)) {
@@ -218,28 +299,43 @@ public class APISistemaDesktop extends Observable{
         }
     }
 
-    /*
-        deletaArquivo exclui o arquivo correspondente ao mes selecionado.
-    */
+    
+    /**
+     * Nao implementado nesse momento.
+     * 
+     * @param mes Informa mes e ano a que se referem os dados que devem ser
+     *            suprimidos.
+     */
     public void deletaArquivo(GregorianCalendar mes){
         
     }
 
     
-    /*
-        comparaMeses simplesmente verifica se dadas duas datas, os meses e anos
-        sao iguais
-    */
+    /**
+     * Verifica se duas datas sao equivalentes levando em consideracao apenas
+     * o mes e o ano.
+     * 
+     * @param a Primeiro operando.
+     * @param b Segundo operando.
+     * @return  true, se a e b tem o ano e mes identicos. false, caso contrario.
+     */
     private boolean comparaMeses(GregorianCalendar a, GregorianCalendar b){
         return (a.get(GregorianCalendar.YEAR) == b.get(GregorianCalendar.YEAR))
                 && (a.get(GregorianCalendar.MONTH) == b.get(GregorianCalendar.MONTH));
     }
     
     
-    /* 
-        Gera um vetor com o historico de gastos para uma dada categoria ao
-        longo de um periodo especifico.
-    */
+    /**
+     * Gera um vetor com o historico de despesas para uma dada categoria ao
+     * longo de um periodo especifico. O resultado e notificado aos observers,
+     * em vez de ser retornado.
+     * 
+     * @param inicio Mes e ano iniciais para o intervalo a ser consultado
+     *               (inicio faz parte do intervalo).
+     * @param fim    Mes e ano finais para o intervalo a ser consultado
+     *               (fim faz parte do intervalo).
+     * @param c      Categoria para a despesa a ser consultada.
+     */
     public void geraRelatorio(GregorianCalendar inicio, GregorianCalendar fim, CategoriaDespesa c) {
         
         // Verifico a entrada
