7c4a7444fd88d61ac3ac0cf7f657d879acfe8b28
lucasmsx <lmdsxavier@inf.ufpel.edu.br>
2016-06-12 15:16:59 -0300

    Parser XML agora insere as informações necessárias no BD. Ainda não realiza verificação de erros e inconformidades.

--- WebVersion/models/Parser.php
+++ WebVersion/models/Parser.php
@@ -9,15 +9,15 @@ class Parser {
 
 	function __construct($arquivo, $email) {
 		$this->xml = simplexml_load_file($arquivo) or die("Error: Cannot create object");
-		$this->email = $email;
 
 		$this->conn = new ConexaoDB();
+		$conexao = $this->conn->abrirConexao();
+		$this->email = mysqli_real_escape_string($conexao, $email);
+		$this->conn->fecharConexao();
 	}
 
 	function parse() {
 		$logfile = "/var/www/html/WebVersion/log.txt";
-		// exec("echo start >> $logfile");
-
 		exec("echo {$this->email} >> $logfile");
 
 		if(strcmp($this->xml->getName(), "financas") != 0) {
@@ -25,20 +25,39 @@ class Parser {
 		}
 
 		foreach($this->xml->children() as $dadosMes) {
+			// Identifica o mes sumbetido
 			$ano = $dadosMes['ano'];
 			$mes = $dadosMes['mes'];
 			exec("echo Dados para $mes de $ano: >> $logfile");
+
+			// Abre uma entrada para o mes submetido no DB
+			$conexao = $this->conn->abrirConexao();
+			$sql = "INSERT INTO DadosMes (mes, usersEmail) VALUES ('{$ano}-{$mes}-01', '{$this->email}');";
+			mysqli_query($conexao,$sql);
+			$this->conn->fecharConexao();
+
+			// Percorre cada uma das movimentacoes registratdas
 			foreach($dadosMes->children() as $movimentacao) {
+				// Extrai os dados
 				$categoria = $movimentacao['categoria'];
 				$tipo = $movimentacao['tipo'];
 				$valor = $movimentacao['valor'];
 				exec("echo $categoria $tipo $valor >> $logfile");
+
+				// Ve se eh despesa ou receita
+				$despesa = 0;
+				if(strcmp($tipo, "despesa") == 0) {
+					$despesa = 1;
+				}
+
+				// Insere no DB
+				$conexao = $this->conn->abrirConexao();
+				$sql = "INSERT INTO Movimentacao (categoria, dadosMesMes, dadosMesUsersEmail, valor, despesa) VALUES ('{$categoria}', '{$ano}-{$mes}-01', '{$this->email}', '{$valor}', '{$despesa}');";
+				mysqli_query($conexao,$sql);
+				$this->conn->fecharConexao();
 			}
 		}
 
-		// Fazer as insercoes
-		// Inserir primeiro em dadosmes: $email, $ano e $mes
-		// Depois iterar em $movimentacoes e inserir
 		return true;
 	}
 }
