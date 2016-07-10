84b5a036fee0d601648b22faf6173e6bc685f1ef
Eliézer Luz Ribeiro <eliezerlribeiro@yahoo.com.br>
2016-07-09 21:25:08 -0300

    Update enviaEmail.php
    retirei um comentario.. Funciona agora

--- WebVersion/controllers/enviaEmail.php
+++ WebVersion/controllers/enviaEmail.php
@@ -36,9 +36,6 @@ function smtpmailer($para, $de, $de_nome, $assunto, $corpo) {
 	}
 }
 
-// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
-o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
-
  if (smtpmailer($Email, 'suport@drefinancas.com', 'suporte drefinancas', 'Troca de senha', $Vai)) {
 
 	echo "tudo certo";
